2338ebe877c71f21ec6dbdcfa986ee0a4bce305e
Katrina McHaffie <katrinaemchaffie@gmail.com>
2017-09-08 20:37:40 -0400

    assignment 2 commit

--- assignment_2/assignment2.js
+++ assignment_2/assignment2.js
@@ -3,7 +3,10 @@ Exercise #1
 Create a for loop that prints out the numbers 1 to 100 in the console.
 */
 
-
+var i;
+for(i=1;i<=100;i++){
+		console.log(i);
+	}
 
 /*
 Exercise #2
@@ -17,3 +20,10 @@ Write a loop that makes seven calls to console.log to output the following trian
 ######
 #######
 */
+
+var triangle = '';
+
+for (var i = 0; i < 7; i++) {
+    triangle = triangle + '#';
+    console.log(triangle);
+}
--- handson1.js
+++ handson1.js
@@ -26,3 +26,16 @@ var a = true;
 var b = false;
 var c = true;
 console.log(a && b && c );
+
+//Exercise #5
+
+var answer1 = 7.25
+var answer2 = 5.25
+
+/*Operator precedence describes the order in which operations are performed when an expression is evaluated. Operations with a higher precedence are performed before those with a lower precedence. For example, multiplication is performed before addition. */
+
+//Exercise #6
+
+var numb = 12
+
+parseFloat() 
